cc81cb0f1f4fe9b4cc78a73c064785cb5c6cf87a
Jose <113186145+jose-yanes@users.noreply.github.com>
2025-01-12 23:21:35 -0300

    Add pending and delete

--- main.py
+++ main.py
@@ -15,7 +15,13 @@ def url():
 
 @app.route("/pending")
 def pending():
-    return render_template("pending.html", pending_links=[])
+    pending_links = read_from_file("pending_urls.txt")
+    return render_template("pending.html", pending_links=pending_links)
+
+@app.route("/delete/<id>")
+def delete(id):
+    print(id)
+    return redirect("/pending")
 
 if __name__ == "__main__":
     app.run(debug=True)
